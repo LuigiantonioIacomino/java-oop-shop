c9ed18d707f960e3552867e67f2a1be026848b3f
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-12-06 16:28:14 +0100

    modifica

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,10 +1,21 @@
 package org.lessons.java.shop;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Prodotto prod=new Prodotto("bici","bella",300,20);
-        System.out.println(prod.getCodice());
-        System.out.println(prod.getPrezzo());
+        Prodotto prod1=new Prodotto("auto","confortevole",300,40);
+        System.out.println(prod1.getPrezzo());
+        System.out.println(prod1.getDescrizione());
+        System.out.println(prod1.getIva());
+        System.out.println(prod1.getNome());
+
+        Prodotto prod2=new Prodotto("motorino","veloce",200,60);
+        System.out.println(prod2.getNome());
+        System.out.println(prod2.getPrezzo());
+        System.out.println(prod2.getDescrizione());
+        System.out.println(prod2.getIva());
+
 
     }
 }
